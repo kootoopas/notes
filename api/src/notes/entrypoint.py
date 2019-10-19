620bfe8573670b32aa89a027654133de01b04828
Fotis Koutoupas <fotis.koutoupas@gmail.com>
2019-10-19 14:55:03 +0300

    Dockerize api

--- api/src/notes/entrypoint.py
+++ api/src/notes/entrypoint.py
@@ -5,5 +5,9 @@ from mongoengine import connect
 from notes import create_flask_application, api
 
 if __name__ == '__main__':
-    with connect(host=os.environ['NOTES_MONGO_URL']):
-        create_flask_application(api).run(debug=True)
+    with connect(host=os.environ['NOTES_MONGO_URL'] or 'mongodb://0.0.0.0:27017/notes'):
+        create_flask_application(api).run(
+            host=os.environ['NOTES_HOST'] or '0.0.0.0',
+            port=int(os.environ['NOTES_PORT']) or 8000,
+            debug=bool(True if os.environ['NOTES_DEBUG'].lower() is 'true' else False) or True
+        )
