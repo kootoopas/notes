91f3a0a6f48b6b3a8764ad02d7c73031a75ae131
Fotis Koutoupas <fotis.koutoupas@gmail.com>
2019-10-22 19:06:28 +0300

    Add jasmine spy type utility

--- ui/src/test/utilities.ts
+++ ui/src/test/utilities.ts
@@ -1,5 +1,9 @@
-import {format} from "date-fns";
+import {format} from 'date-fns'
+// @ts-ignore
+import Spy = jasmine.Spy;
 
 export function getDateString(date: Date): string {
   return format(date, 'yyyy-MM-dd HH:mm:ss:SSS')
 }
+
+export const asSpy = f => f as Spy
