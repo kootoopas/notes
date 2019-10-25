df69b3d6266938a7c8574234672c77e5a28ef751
Fotis Koutoupas <fotis.koutoupas@gmail.com>
2019-10-25 15:13:16 +0300

    Test app component

--- ui/src/app/app.component.spec.ts
+++ ui/src/app/app.component.spec.ts
@@ -1,23 +1,32 @@
-import { TestBed, async } from '@angular/core/testing';
+import {TestBed, async, ComponentFixture} from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-import {NoteModule} from './note/note.module';
 import {provideMockStore} from '@ngrx/store/testing';
 import {Store} from '@ngrx/store';
 import {RootState} from './reducers';
 import {initialState as noteInitialState, noteFeatureKey} from './note/note.reducers';
+import {Component, DebugElement} from '@angular/core';
+import {loadNotes} from './note/note.actions';
 
 describe('AppComponent', () => {
   let store: Store<RootState>
+  let fixture: ComponentFixture<AppComponent>
+
+  @Component({ selector: 'app-note-editor', template: '' })
+  class NoteEditorStubComponent {}
+
+  @Component({ selector: 'app-note-browser', template: '' })
+  class NoteBrowserStubComponent {}
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule,
-        NoteModule
+        RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        NoteEditorStubComponent,
+        NoteBrowserStubComponent
       ],
       providers: [
         provideMockStore({
@@ -26,12 +35,25 @@ describe('AppComponent', () => {
           }
         })
       ]
-    }).compileComponents();
-  }));
+    }).compileComponents()
+  }))
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent)
+    store = TestBed.get<Store<RootState>>(Store)
+  })
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
-});
+    const app = fixture.debugElement.componentInstance
+    expect(app).toBeTruthy()
+  })
+
+  it('should load notes on initialization', () => {
+    const dispatchSpy = spyOn(store, 'dispatch')
+
+    fixture.detectChanges()
+
+    expect(dispatchSpy).toHaveBeenCalledTimes(1)
+    expect(dispatchSpy).toHaveBeenCalledWith(loadNotes())
+  })
+})
